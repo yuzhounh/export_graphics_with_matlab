55c9a0efbb5e0d4eb5242b190947183483484ffe
Jing Wang <yuzhounh@163.com>
2024-09-26 15:05:04 +0800

    Add files via upload

--- export_graphics.m
+++ export_graphics.m
@@ -1,4 +1,4 @@
-function export_graphics(folder_name, figure_name, resolution)
+function export_graphics(figure_handle, folder_name, figure_name, resolution)
 % create a folder to save the graphics
 remkdir(folder_name);
 
@@ -7,153 +7,153 @@ folder = sprintf('%s/method_1_exportgraphics', folder_name);
 remkdir(folder);
 
 % file path
-output_filepath_jpg = sprintf('%s/%s.jpg', folder, figure_name);
-output_filepath_png = sprintf('%s/%s.png', folder, figure_name);
-output_filepath_tif = sprintf('%s/%s.tif', folder, figure_name);
-output_filepath_gif = sprintf('%s/%s.gif', folder, figure_name);
-output_filepath_bmp = sprintf('%s/%s.bmp', folder, figure_name);
-output_filepath_eps = sprintf('%s/%s.eps', folder, figure_name);
-output_filepath_pdf = sprintf('%s/%s.pdf', folder, figure_name);
-output_filepath_svg = sprintf('%s/%s.svg', folder, figure_name);
-output_filepath_emf = sprintf('%s/%s.emf', folder, figure_name);
-output_filepath_fig = sprintf('%s/%s.fig', folder, figure_name);
+figure_jpg = sprintf('%s/%s.jpg', folder, figure_name);
+figure_png = sprintf('%s/%s.png', folder, figure_name);
+figure_tif = sprintf('%s/%s.tif', folder, figure_name);
+figure_gif = sprintf('%s/%s.gif', folder, figure_name);
+figure_bmp = sprintf('%s/%s.bmp', folder, figure_name);
+figure_eps = sprintf('%s/%s.eps', folder, figure_name);
+figure_pdf = sprintf('%s/%s.pdf', folder, figure_name);
+figure_svg = sprintf('%s/%s.svg', folder, figure_name);
+figure_emf = sprintf('%s/%s.emf', folder, figure_name);
+figure_fig = sprintf('%s/%s.fig', folder, figure_name);
 
 % exportgraphics
-exportgraphics(gcf, output_filepath_jpg, 'Resolution', resolution);
-exportgraphics(gcf, output_filepath_png, 'Resolution', resolution);
-exportgraphics(gcf, output_filepath_tif, 'Resolution', resolution);
-exportgraphics(gcf, output_filepath_gif, 'Resolution', resolution);
-% exportgraphics(gcf, output_filepath_bmp, 'Resolution', resolution);
-exportgraphics(gcf, output_filepath_eps, 'ContentType', 'vector');
-exportgraphics(gcf, output_filepath_pdf, 'ContentType', 'vector');
-% exportgraphics(gcf, output_filepath_svg, 'ContentType', 'vector');
-exportgraphics(gcf, output_filepath_emf, 'ContentType', 'vector');
-% exportgraphics(gcf, output_filepath_fig, 'ContentType', 'vector');
+exportgraphics(figure_handle, figure_jpg, 'Resolution', resolution);
+exportgraphics(figure_handle, figure_png, 'Resolution', resolution);
+exportgraphics(figure_handle, figure_tif, 'Resolution', resolution);
+exportgraphics(figure_handle, figure_gif, 'Resolution', resolution);
+% exportgraphics(figure_handle, figure_bmp, 'Resolution', resolution);
+exportgraphics(figure_handle, figure_eps, 'ContentType', 'vector');
+exportgraphics(figure_handle, figure_pdf, 'ContentType', 'vector');
+% exportgraphics(figure_handle, figure_svg, 'ContentType', 'vector');
+exportgraphics(figure_handle, figure_emf, 'ContentType', 'vector');
+% exportgraphics(figure_handle, figure_fig, 'ContentType', 'vector');
 
 %% print
 folder = sprintf('%s/method_2_print', folder_name); 
 remkdir(folder);
 
 % file path
-output_filepath_jpg = sprintf('%s/%s.jpg', folder, figure_name);
-output_filepath_png = sprintf('%s/%s.png', folder, figure_name);
-output_filepath_tif = sprintf('%s/%s.tif', folder, figure_name);
-output_filepath_gif = sprintf('%s/%s.gif', folder, figure_name);
-output_filepath_bmp = sprintf('%s/%s.bmp', folder, figure_name);
-output_filepath_eps = sprintf('%s/%s.eps', folder, figure_name);
-output_filepath_pdf = sprintf('%s/%s.pdf', folder, figure_name);
-output_filepath_svg = sprintf('%s/%s.svg', folder, figure_name);
-output_filepath_emf = sprintf('%s/%s.emf', folder, figure_name);
-output_filepath_fig = sprintf('%s/%s.fig', folder, figure_name);
+figure_jpg = sprintf('%s/%s.jpg', folder, figure_name);
+figure_png = sprintf('%s/%s.png', folder, figure_name);
+figure_tif = sprintf('%s/%s.tif', folder, figure_name);
+figure_gif = sprintf('%s/%s.gif', folder, figure_name);
+figure_bmp = sprintf('%s/%s.bmp', folder, figure_name);
+figure_eps = sprintf('%s/%s.eps', folder, figure_name);
+figure_pdf = sprintf('%s/%s.pdf', folder, figure_name);
+figure_svg = sprintf('%s/%s.svg', folder, figure_name);
+figure_emf = sprintf('%s/%s.emf', folder, figure_name);
+figure_fig = sprintf('%s/%s.fig', folder, figure_name);
 
 % print
-print(gcf, output_filepath_jpg, '-djpeg', sprintf('-r%d', resolution));
-print(gcf, output_filepath_png, '-dpng', sprintf('-r%d', resolution));
-print(gcf, output_filepath_tif, '-dtiff', sprintf('-r%d', resolution));  
-% print(gcf, output_filepath_gif, '-dgif', sprintf('-r%d', resolution));  
-% print(gcf, output_filepath_bmp, '-dbmp', sprintf('-r%d', resolution));  
-print(gcf, output_filepath_emf, '-dmeta', sprintf('-r%d', resolution));
-print(gcf, output_filepath_pdf, '-dpdf', '-vector'); 
-print(gcf, output_filepath_eps, '-depsc', '-vector'); 
-print(gcf, output_filepath_svg, '-dsvg', '-vector');
-print(gcf, output_filepath_emf, '-dmeta', '-vector');
-% print(gcf, output_filepath_fig, '-dfig', '-vector');
+print(figure_handle, figure_jpg, '-djpeg', sprintf('-r%d', resolution));
+print(figure_handle, figure_png, '-dpng', sprintf('-r%d', resolution));
+print(figure_handle, figure_tif, '-dtiff', sprintf('-r%d', resolution));  
+% print(figure_handle, figure_gif, '-dgif', sprintf('-r%d', resolution));  
+% print(figure_handle, figure_bmp, '-dbmp', sprintf('-r%d', resolution));  
+print(figure_handle, figure_emf, '-dmeta', sprintf('-r%d', resolution));
+print(figure_handle, figure_pdf, '-dpdf', '-vector'); 
+print(figure_handle, figure_eps, '-depsc', '-vector'); 
+print(figure_handle, figure_svg, '-dsvg', '-vector');
+% print(figure_handle, figure_emf, '-dmeta', '-vector');
+% print(figure_handle, figure_fig, '-dfig', '-vector');
 
 %% saveas
 folder = sprintf('%s/method_3_saveas', folder_name); 
 remkdir(folder);
 
 % file path
-output_filepath_jpg = sprintf('%s/%s.jpg', folder, figure_name);
-output_filepath_png = sprintf('%s/%s.png', folder, figure_name);
-output_filepath_tif = sprintf('%s/%s.tif', folder, figure_name);
-output_filepath_gif = sprintf('%s/%s.gif', folder, figure_name);
-output_filepath_bmp = sprintf('%s/%s.bmp', folder, figure_name);
-output_filepath_eps = sprintf('%s/%s.eps', folder, figure_name);
-output_filepath_pdf = sprintf('%s/%s.pdf', folder, figure_name);
-output_filepath_svg = sprintf('%s/%s.svg', folder, figure_name);
-output_filepath_emf = sprintf('%s/%s.emf', folder, figure_name);
-output_filepath_fig = sprintf('%s/%s.fig', folder, figure_name);
+figure_jpg = sprintf('%s/%s.jpg', folder, figure_name);
+figure_png = sprintf('%s/%s.png', folder, figure_name);
+figure_tif = sprintf('%s/%s.tif', folder, figure_name);
+figure_gif = sprintf('%s/%s.gif', folder, figure_name);
+figure_bmp = sprintf('%s/%s.bmp', folder, figure_name);
+figure_eps = sprintf('%s/%s.eps', folder, figure_name);
+figure_pdf = sprintf('%s/%s.pdf', folder, figure_name);
+figure_svg = sprintf('%s/%s.svg', folder, figure_name);
+figure_emf = sprintf('%s/%s.emf', folder, figure_name);
+figure_fig = sprintf('%s/%s.fig', folder, figure_name);
 
-saveas(gcf, output_filepath_jpg);
-saveas(gcf, output_filepath_png);
-saveas(gcf, output_filepath_tif); 
-% saveas(gcf, output_filepath_gif); 
-saveas(gcf, output_filepath_bmp); 
-saveas(gcf, output_filepath_eps);
-saveas(gcf, output_filepath_pdf); 
-saveas(gcf, output_filepath_svg); 
-saveas(gcf, output_filepath_emf); 
-saveas(gcf, output_filepath_fig);
+saveas(figure_handle, figure_jpg);
+saveas(figure_handle, figure_png);
+saveas(figure_handle, figure_tif); 
+% saveas(figure_handle, figure_gif); 
+saveas(figure_handle, figure_bmp); 
+saveas(figure_handle, figure_eps);
+saveas(figure_handle, figure_pdf); 
+saveas(figure_handle, figure_svg); 
+saveas(figure_handle, figure_emf); 
+saveas(figure_handle, figure_fig);
 
 %% export_fig
 folder = sprintf('%s/method_4_exportfig', folder_name); 
 remkdir(folder);
 
 % file path
-output_filepath_jpg = sprintf('%s/%s.jpg', folder, figure_name);
-output_filepath_png = sprintf('%s/%s.png', folder, figure_name);
-output_filepath_tif = sprintf('%s/%s.tif', folder, figure_name);
-output_filepath_gif = sprintf('%s/%s.gif', folder, figure_name);
-output_filepath_bmp = sprintf('%s/%s.bmp', folder, figure_name);
-output_filepath_eps = sprintf('%s/%s.eps', folder, figure_name);
-output_filepath_pdf = sprintf('%s/%s.pdf', folder, figure_name);
-output_filepath_svg = sprintf('%s/%s.svg', folder, figure_name);
-output_filepath_emf = sprintf('%s/%s.emf', folder, figure_name);
-output_filepath_fig = sprintf('%s/%s.fig', folder, figure_name);
+figure_jpg = sprintf('%s/%s.jpg', folder, figure_name);
+figure_png = sprintf('%s/%s.png', folder, figure_name);
+figure_tif = sprintf('%s/%s.tif', folder, figure_name);
+figure_gif = sprintf('%s/%s.gif', folder, figure_name);
+figure_bmp = sprintf('%s/%s.bmp', folder, figure_name);
+figure_eps = sprintf('%s/%s.eps', folder, figure_name);
+figure_pdf = sprintf('%s/%s.pdf', folder, figure_name);
+figure_svg = sprintf('%s/%s.svg', folder, figure_name);
+figure_emf = sprintf('%s/%s.emf', folder, figure_name);
+figure_fig = sprintf('%s/%s.fig', folder, figure_name);
 
-export_fig(output_filepath_jpg, '-jpeg', sprintf('-r%d', resolution), '-transparent');
-export_fig(output_filepath_png, '-png', sprintf('-r%d', resolution), '-transparent');
-export_fig(output_filepath_tif, '-tiff', sprintf('-r%d', resolution), '-transparent');
-export_fig(output_filepath_gif, '-gif', sprintf('-r%d', resolution), '-transparent');
-export_fig(output_filepath_bmp, '-bmp', sprintf('-r%d', resolution), '-transparent');
-export_fig(output_filepath_eps, '-eps', '-painters', '-transparent');
-export_fig(output_filepath_pdf, '-pdf', '-painters', '-transparent');
-export_fig(output_filepath_svg, '-svg', '-painters', '-transparent');
-export_fig(output_filepath_emf, '-emf', '-painters', '-transparent');
-export_fig(output_filepath_fig, '-fig', '-painters', '-transparent');
+export_fig(figure_handle, figure_jpg, '-jpeg', sprintf('-r%d', resolution), '-transparent');
+export_fig(figure_handle, figure_png, '-png', sprintf('-r%d', resolution), '-transparent');
+export_fig(figure_handle, figure_tif, '-tiff', sprintf('-r%d', resolution), '-transparent');
+export_fig(figure_handle, figure_gif, '-gif', sprintf('-r%d', resolution), '-transparent');
+export_fig(figure_handle, figure_bmp, '-bmp', sprintf('-r%d', resolution), '-transparent');
+export_fig(figure_handle, figure_eps, '-eps', '-painters', '-transparent');
+export_fig(figure_handle, figure_pdf, '-pdf', '-painters', '-transparent');
+export_fig(figure_handle, figure_svg, '-svg', '-painters', '-transparent');
+export_fig(figure_handle, figure_emf, '-emf', '-painters', '-transparent');
+export_fig(figure_handle, figure_fig, '-fig', '-painters', '-transparent');
 
 %% fig2svg
 folder = sprintf('%s/method_5_fig2svg', folder_name); 
 remkdir(folder);
 
 % file path
-output_filepath_jpg = sprintf('%s/%s.jpg', folder, figure_name);
-output_filepath_png = sprintf('%s/%s.png', folder, figure_name);
-output_filepath_tif = sprintf('%s/%s.tif', folder, figure_name);
-output_filepath_gif = sprintf('%s/%s.gif', folder, figure_name);
-output_filepath_bmp = sprintf('%s/%s.bmp', folder, figure_name);
-output_filepath_eps = sprintf('%s/%s.eps', folder, figure_name);
-output_filepath_pdf = sprintf('%s/%s.pdf', folder, figure_name);
-output_filepath_svg = sprintf('%s/%s.svg', folder, figure_name);
-output_filepath_emf = sprintf('%s/%s.emf', folder, figure_name);
-output_filepath_fig = sprintf('%s/%s.fig', folder, figure_name);
+figure_jpg = sprintf('%s/%s.jpg', folder, figure_name);
+figure_png = sprintf('%s/%s.png', folder, figure_name);
+figure_tif = sprintf('%s/%s.tif', folder, figure_name);
+figure_gif = sprintf('%s/%s.gif', folder, figure_name);
+figure_bmp = sprintf('%s/%s.bmp', folder, figure_name);
+figure_eps = sprintf('%s/%s.eps', folder, figure_name);
+figure_pdf = sprintf('%s/%s.pdf', folder, figure_name);
+figure_svg = sprintf('%s/%s.svg', folder, figure_name);
+figure_emf = sprintf('%s/%s.emf', folder, figure_name);
+figure_fig = sprintf('%s/%s.fig', folder, figure_name);
 
-% fig2svg(output_filepath_jpg);
-% fig2svg(output_filepath_png);
-% fig2svg(output_filepath_tif);
-% fig2svg(output_filepath_gif);
-% fig2svg(output_filepath_bmp);
-% fig2svg(output_filepath_eps);
-% fig2svg(output_filepath_pdf);
-fig2svg(output_filepath_svg);
-% fig2svg(output_filepath_emf);
-% fig2svg(output_filepath_fig);
+% export_fig(figure_handle, figure_jpg);
+% export_fig(figure_handle, figure_png);
+% export_fig(figure_handle, figure_tif);
+% export_fig(figure_handle, figure_gif);
+% export_fig(figure_handle, figure_bmp);
+% export_fig(figure_handle, figure_eps);
+% export_fig(figure_handle, figure_pdf);
+export_fig(figure_handle, figure_svg);
+% export_fig(figure_handle, figure_emf);
+% export_fig(figure_handle, figure_fig);
 
 %% savefig
 folder = sprintf('%s/method_6_savefig', folder_name); 
 remkdir(folder);
 
 % file path
-output_filepath_jpg = sprintf('%s/%s.jpg', folder, figure_name);
-output_filepath_png = sprintf('%s/%s.png', folder, figure_name);
-output_filepath_tif = sprintf('%s/%s.tif', folder, figure_name);
-output_filepath_gif = sprintf('%s/%s.gif', folder, figure_name);
-output_filepath_bmp = sprintf('%s/%s.bmp', folder, figure_name);
-output_filepath_eps = sprintf('%s/%s.eps', folder, figure_name);
-output_filepath_pdf = sprintf('%s/%s.pdf', folder, figure_name);
-output_filepath_svg = sprintf('%s/%s.svg', folder, figure_name);
-output_filepath_emf = sprintf('%s/%s.emf', folder, figure_name);
-output_filepath_fig = sprintf('%s/%s.fig', folder, figure_name);
+figure_jpg = sprintf('%s/%s.jpg', folder, figure_name);
+figure_png = sprintf('%s/%s.png', folder, figure_name);
+figure_tif = sprintf('%s/%s.tif', folder, figure_name);
+figure_gif = sprintf('%s/%s.gif', folder, figure_name);
+figure_bmp = sprintf('%s/%s.bmp', folder, figure_name);
+figure_eps = sprintf('%s/%s.eps', folder, figure_name);
+figure_pdf = sprintf('%s/%s.pdf', folder, figure_name);
+figure_svg = sprintf('%s/%s.svg', folder, figure_name);
+figure_emf = sprintf('%s/%s.emf', folder, figure_name);
+figure_fig = sprintf('%s/%s.fig', folder, figure_name);
 
-savefig(output_filepath_fig);
+savefig(figure_handle, figure_fig);
--- main.m
+++ main.m
@@ -5,7 +5,7 @@ addpath(genpath('fig2svg-master\'));
 addpath(genpath('export_fig-master\'));
 
 % Create figure
-figure;
+figure_handle = figure;
 plot(1:10, rand(1, 10));
 xlabel('X label');
 ylabel('Y label');
@@ -17,8 +17,8 @@ pos=get(gcf,'Position');
 scale=0.7;
 set(gcf,'Position',[pos(1),pos(2),pos(3)*scale,pos(4)*scale]);
 
-% Export figures
+% Export graphics
 folder_name = 'figures';
 figure_name = 'figure_1';  
 resolution = 300; 
-export_graphics(folder_name, figure_name, resolution);
+export_graphics(figure_handle, folder_name, figure_name, resolution);
